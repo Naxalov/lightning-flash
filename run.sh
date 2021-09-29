12e3ecc4ce01010cf22789976140f1caf59be527
ZARIF ubuntu <uzbzarif@gmail.com>
2021-09-29 21:28:23 +0900

    run

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --ipc=host --rm --runtime=nvidia -it -p 8888:8888 -v '$(pwd)/workspace:/workspace' flash bash
+docker run --ipc=host --rm --runtime=nvidia -it -p 8888:8888 -v $(pwd)/workspace:'/workspace' flash bash
